07ec3892aa4be7e6b323b4051d586de11f4e4d33
Maya Saxena <maya.saxena@gmail.com>
2018-03-23 15:57:35 -0400

    Implement Step 1

--- PickPocket/Models/Lock.swift
+++ PickPocket/Models/Lock.swift
@@ -9,7 +9,40 @@
 import Foundation
 
 struct Lock {
+    private let code: String
+
+    init(code: String) {
+        self.code = code
+    }
+
     func submit(guess: String) -> GuessResult {
-        return GuessResult(correct: 0, misplaced: 0)
+        var correct = 0
+        var misplaced = 0
+        var codeUnmatched = [Character: Int]()
+        var guessUnmatched = [Character: Int]()
+
+        zip(code, guess).forEach { (codeCharacter, guessCharacter) in
+            if codeCharacter == guessCharacter {
+                correct += 1
+            } else {
+                if let guessUnmatchedCount = guessUnmatched[codeCharacter], guessUnmatchedCount > 0 {
+                    guessUnmatched[codeCharacter] = guessUnmatchedCount - 1
+                    misplaced += 1
+                } else {
+                    let codeUnmatchedCount = codeUnmatched[codeCharacter] ?? 0
+                    codeUnmatched[codeCharacter] = codeUnmatchedCount + 1
+                }
+
+                if let codeUnmatchedCount = codeUnmatched[guessCharacter], codeUnmatchedCount > 0 {
+                    codeUnmatched[guessCharacter] = codeUnmatchedCount - 1
+                    misplaced += 1
+                } else {
+                    let guessUnmatchedCount = guessUnmatched[codeCharacter] ?? 0
+                    guessUnmatched[guessCharacter] = guessUnmatchedCount + 1
+                }
+            }
+        }
+
+        return GuessResult(correct: correct, misplaced: misplaced)
     }
 }
