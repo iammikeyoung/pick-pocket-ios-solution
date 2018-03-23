218d4ed0a6e2792a94b6c64239b7b44ecb7c3e60
Maya Saxena <maya.saxena@gmail.com>
2018-03-23 15:57:35 -0400

    Implement Step 1

--- PickPocketTests/LockTests.swift
+++ PickPocketTests/LockTests.swift
@@ -12,9 +12,114 @@ import XCTest
 class LockTests: XCTestCase {
 
     func testCorrectGuess() {
-        let sut = Lock()
+        let sut = Lock(code: "123")
         let result = sut.submit(guess: "123")
         XCTAssertEqual(result.correct, 3)
+        XCTAssertEqual(result.misplaced, 0)
+    }
+
+    func testOneCorrectOneMisplaced() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "134")
+        XCTAssertEqual(result.correct, 1)
+        XCTAssertEqual(result.misplaced, 1)
+    }
+
+    func testOneCorrect() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "156")
+        XCTAssertEqual(result.correct, 1)
+        XCTAssertEqual(result.misplaced, 0)
+    }
+
+    func testOneMisplaced() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "414")
+        XCTAssertEqual(result.correct, 0)
+        XCTAssertEqual(result.misplaced, 1)
+    }
+
+    func testNoneCorrect() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "456")
+        XCTAssertEqual(result.correct, 0)
+        XCTAssertEqual(result.misplaced, 0)
+    }
+
+    func testAllMisplaced() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "312")
+        XCTAssertEqual(result.correct, 0)
+        XCTAssertEqual(result.misplaced, 3)
+    }
+
+    func testDuplicatesGuess() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "122")
+        XCTAssertEqual(result.correct, 2)
+        XCTAssertEqual(result.misplaced, 0)
+    }
+
+    func testDuplicateMisplaced() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "411")
+        XCTAssertEqual(result.correct, 0)
+        XCTAssertEqual(result.misplaced, 1)
+    }
+
+    func testPrecedence() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "114")
+        XCTAssertEqual(result.correct, 1)
+        XCTAssertEqual(result.misplaced, 0)
+    }
+
+    func testCorrectPrecedence() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "433")
+        XCTAssertEqual(result.correct, 1)
+        XCTAssertEqual(result.misplaced, 0)
+    }
+
+    func testDuplicatesCode() {
+        let sut = Lock(code: "2245")
+        let result = sut.submit(guess: "1122")
+        XCTAssertEqual(result.correct, 0)
+        XCTAssertEqual(result.misplaced, 2)
+    }
+
+    func testMultiplesMisplaced() {
+        let sut = Lock(code: "323")
+        let result = sut.submit(guess: "232")
+        XCTAssertEqual(result.correct, 0)
+        XCTAssertEqual(result.misplaced, 2)
+    }
+
+    func testMultiplesMixed() {
+        let sut = Lock(code: "323")
+        let result = sut.submit(guess: "332")
+        XCTAssertEqual(result.correct, 1)
+        XCTAssertEqual(result.misplaced, 2)
+    }
+
+    func testLengthMismatch() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "12")
+        XCTAssertEqual(result.correct, 2)
+        XCTAssertEqual(result.misplaced, 0)
+    }
+
+    func testEmpty() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "")
+        XCTAssertEqual(result.correct, 0)
+        XCTAssertEqual(result.misplaced, 0)
+    }
+
+    func testNonNumeric() {
+        let sut = Lock(code: "123")
+        let result = sut.submit(guess: "abc")
+        XCTAssertEqual(result.correct, 0)
         XCTAssertEqual(result.misplaced, 0)
     }
 }
